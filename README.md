d7ad1d5ef7a570fd3a847c7142309d1dd10bb4a2
bartlomiejkaminski <kamin.bart@gmail.com>
2016-12-20 19:22:54 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ColumnRectangleEC2
---
+++ src/main/java/constructions/column_rectangle_ec2/App.java
@@ -0,0 +1,13 @@
+package constructions.column_rectangle_ec2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
